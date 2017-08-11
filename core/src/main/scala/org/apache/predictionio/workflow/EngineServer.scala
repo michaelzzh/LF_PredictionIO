f0900ce1209d454d66f8ee434a66ec13efa7e7e1
ChrisYG <yg012014@mymail.pomona.edu>
2017-08-11 09:56:12 -0700

    Keep deleting histories until only 10 left

--- core/src/main/scala/org/apache/predictionio/workflow/EngineServer.scala
+++ core/src/main/scala/org/apache/predictionio/workflow/EngineServer.scala
@@ -608,13 +608,16 @@ class EngineServerActor[Q, P](
                                             predictions = responseList,
                                             finishTime = groupHistory.finishTime)
                     // remove queryhistories & querygrouphistories if more than 10 groups exist
+                    // until 10 left?
                     queryGroupHistories.count() match {
                       case Some(value : Int) => {
-                        if (value > 10) {
+                        var count = value
+                        while (count > 10) {
                         //queryGroupHistories.delete(groupHistory.groupId, groupHistory.engineId)
                           queryGroupHistories.deleteOldest() match {
                             case Some(value : String) => queryHistories.getGroup(value).map(qh => queryHistories.delete(qh.queryId, value))
-                          } 
+                          }
+                          count = count-1
                         }
                       }
                     }
